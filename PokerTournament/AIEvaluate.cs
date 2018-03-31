74c1a62911c74df1a554ffb1ad8e49842e84e32c
tylerac7622 <tac7622@g.rit.edu>
2018-03-31 14:07:51 -0400

    Added a function called PotentialRank which returns the best combo a hand is close to

--- PokerTournament/AIEvaluate.cs
+++ PokerTournament/AIEvaluate.cs
@@ -8,6 +8,7 @@ namespace PokerTournament
 {
     class AIEvaluate
     {
+        //list of helper functions telling how close a hand is to certain combos
         public static int CloseToRoyalFlush(Card[] hand)
         {
             // sort the hand
@@ -17,14 +18,18 @@ namespace PokerTournament
             int maxLength = 0;
             int currentLength = 0;
             int currentTracker = hand[0].Value;
+            //loops through each card, checking if it is the earliest point of a possible straight
             for (int i = 0; i < 5; i++)
             {
+                //royal flush must start at 10
                 if (hand[i].Value > 9)
                 {
                     currentTracker = hand[i].Value;
                     currentLength = 1;
+                    //check each card after to see if it could be a part of a straight including the first card
                     for (int i2 = i + 1; i2 < 5; i2++)
                     {
+                        //another card is a part of the straight flush if it is within 5 value of the start and isn't a repeat value of the previous card in the straight and the suit is the same as the first card
                         if (hand[i2].Value - hand[i].Value < 5 && hand[i2].Value != currentTracker && hand[i2].Suit == hand[i].Suit)
                         {
                             currentLength++;
@@ -32,6 +37,7 @@ namespace PokerTournament
                         }
                     }
                 }
+                //the largest potential straight that can be found is returned
                 if (currentLength > maxLength)
                 {
                     maxLength = currentLength;
@@ -45,21 +51,25 @@ namespace PokerTournament
             Card highCard = null;
             int rank = Evaluate.RateAHand(hand, out highCard);
 
-            int maxLength = 1;
+            int maxLength = 0;
             int currentLength = 0;
             int currentTracker = hand[0].Value;
+            //loops through each card, checking if it is the earliest point of a possible straight
             for (int i = 0; i < 5; i++)
             {
                 currentTracker = hand[i].Value;
                 currentLength = 1;
+                //check each card after to see if it could be a part of a straight including the first card
                 for (int i2 = i + 1; i2 < 5; i2++)
                 {
+                    //another card is a part of the straight flush if it is within 5 value of the start and isn't a repeat value of the previous card in the straight and the suit is the same as the first card
                     if (hand[i2].Value - hand[i].Value < 5 && hand[i2].Value != currentTracker && hand[i2].Suit == hand[i].Suit)
                     {
                         currentLength++;
                         currentTracker = hand[i2].Value;
                     }
                 }
+                //the largest potential straight that can be found is returned
                 if (currentLength > maxLength)
                 {
                     maxLength = currentLength;
@@ -73,29 +83,32 @@ namespace PokerTournament
             Card highCard = null;
             int rank = Evaluate.RateAHand(hand, out highCard);
 
-            int maxLength = 1;
+            int maxLength = 0;
             int currentLength = 0;
             int currentTracker = hand[0].Value;
+            //loops through each card, checking if it is the earliest point of a possible straight
             for (int i = 0; i < 5; i++)
             {
                 currentTracker = hand[i].Value;
                 currentLength = 1;
-                for (int i2 = i+1; i2 < 5; i2++)
+                //check each card after to see if it could be a part of a straight including the first card
+                for (int i2 = i + 1; i2 < 5; i2++)
                 {
+                    //another card is a part of the straight if it is within 5 value of the start and isn't a repeat value of the previous card in the straight
                     if (hand[i2].Value - hand[i].Value < 5 && hand[i2].Value != currentTracker)
                     {
                         currentLength++;
                         currentTracker = hand[i2].Value;
                     }
                 }
-                if(currentLength > maxLength)
+                //the largest potential straight that can be found is returned
+                if (currentLength > maxLength)
                 {
                     maxLength = currentLength;
                 }
             }
             return maxLength;
         }
-
         public static int CloseToFlush(Card[] hand)
         {
             // sort the hand
@@ -103,6 +116,7 @@ namespace PokerTournament
 
             int maxLength = 1;
 
+            //returns the largest count of each suit, since that suit will be closest to a flush
             int diamonds = AllSameSuit(hand, "Diamonds");
             int spades = AllSameSuit(hand, "Spades");
             int clubs = AllSameSuit(hand, "Clubs");
@@ -112,7 +126,7 @@ namespace PokerTournament
 
             return maxLength;
         }
-
+        //returns the greatest "-of-a-kind" combo currently in the hand
         public static int MostOfSameValue(Card[] hand)
         {
             int mostCount = 1;
@@ -125,37 +139,23 @@ namespace PokerTournament
             }
             return mostCount;
         }
-
-        public static int AllSameSuit(Card[] hand, string suit)
-        {
-            int returnCount = 0;
-            for (int i = 0; i < 5; i++)
-            {
-                if (hand[i].Suit == suit)
-                {
-                    returnCount++;
-                }
-            }
-            return returnCount;
-        }
-
         public static int CloseToFullHouse(Card[] hand)
         {
             int chosen = 0;
             int firstPairingCount = 0;
-            for(int i = 0; i < 5; i++)
+            for (int i = 0; i < 5; i++)
             {
-                if(Evaluate.ValueCount(i, hand) > 1)
+                if (Evaluate.ValueCount(i, hand) > 1)
                 {
                     chosen = hand[i].Value;
                     firstPairingCount = Evaluate.ValueCount(i, hand);
                 }
             }
-            if(firstPairingCount == 0)
+            if (firstPairingCount == 0)
             {
-                return 0;
+                return 2;
             }
-            int secondPairingCount = 0;
+            int secondPairingCount = 1;
             for (int i = 0; i < 5; i++)
             {
                 if (Evaluate.ValueCount(i, hand) > 1 && hand[i].Value != chosen)
@@ -165,6 +165,7 @@ namespace PokerTournament
             }
             return firstPairingCount + secondPairingCount;
         }
+        //like the fullhouse one, but only checking for 2-2 (and thus locking 3 of a kind to 2)
         public static int CloseToTwoPair(Card[] hand)
         {
             int chosen = 0;
@@ -179,9 +180,9 @@ namespace PokerTournament
             }
             if (firstPairingCount == 0)
             {
-                return 0;
+                return 2;
             }
-            int secondPairingCount = 0;
+            int secondPairingCount = 1;
             for (int i = 0; i < 5; i++)
             {
                 if (Evaluate.ValueCount(i, hand) > 1 && hand[i].Value != chosen)
@@ -190,6 +191,57 @@ namespace PokerTournament
                 }
             }
             return firstPairingCount + secondPairingCount;
+        }
+
+        //helper function returning the number of cards in a hand that are of the passed in suit
+        public static int AllSameSuit(Card[] hand, string suit)
+        {
+            int returnCount = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (hand[i].Suit == suit)
+                {
+                    returnCount++;
+                }
+            }
+            return returnCount;
+        }
+
+        //This function returns the rank of potential hand based on how close the current hand is to combos
+        public static int PotentialRank(Card[] hand)
+        {
+            //how much is needed to be close to a full hand combo
+            int needForFive = 3;
+
+            if(CloseToRoyalFlush(hand) >= needForFive)
+            {
+                return 10;
+            }
+            if (CloseToStraightFlush(hand) >= needForFive)
+            {
+                return 9;
+            }
+            if (MostOfSameValue(hand) >= 3)
+            {
+                return 8;
+            }
+            if (CloseToFullHouse(hand) >= needForFive)
+            {
+                return 7;
+            }
+            if (CloseToFlush(hand) >= needForFive)
+            {
+                return 6;
+            }
+            if (CloseToStraight(hand) >= needForFive)
+            {
+                return 5;
+            }
+            if (MostOfSameValue(hand) == 2)
+            {
+                return 4;
+            }
+            return 3; //two pair is the worst potential hand that is returned
         }
 
         public static void ListTheHand(Card[] hand, String name)
@@ -208,6 +260,7 @@ namespace PokerTournament
             Console.WriteLine("\n\t " + CloseToRoyalFlush(hand));
         }
 
+        //returns the string version of a rank, for prettier outputs
         public static String PrintRank(int rank)
         {
             switch (rank)
@@ -252,6 +305,7 @@ namespace PokerTournament
             return "High Card";
         }
 
+        //returns the last action by a player, or null if there is no previous action
         public static PlayerAction LastAction(List<PlayerAction> actions)
         {
             if(actions.Count > 0)
@@ -263,6 +317,8 @@ namespace PokerTournament
                 return null;
             }
         }
+
+        //returns the current bet requirement, or 0 if no one has bet yet
         public static int CurrentBet(List<PlayerAction> actions)
         {
             int actionIter = actions.Count - 1;
